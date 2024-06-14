edcd14ee3d627ff776da154e038818f4f5c434e3
Atharva pawar <atharvapawar2018@gmail.com>
2024-06-14 21:14:03 +0530

    Layout and Styling of Navbar Component completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
-import Wrapper from './sections/Wrapper'
 import Footer from './sections/Footer'
 import Background from './components/Background'
 import "./scss/index.scss";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Search from './pages/Search'
+import MyList from './pages/MyList'
+import About from './pages/About'
+import Compare from './pages/Compare'
+import Pokemon from './pages/Pokemon'
 
-function App (){
+function App() {
   return (
     <div className="main-container">
       <Background />
-      <div className="app">
-        <Navbar />
-        <Wrapper />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <Navbar />
+          <Routes>
+              <Route element={<Search />} path="/search" />
+              <Route element={<MyList />} path="/list" />
+              <Route element={<About />} path="/about" />
+              <Route element={<Compare />} path="/compare" />
+              <Route element={<Pokemon />} path="/pokemon/:id" />
+              <Route element={<Navigate to="/pokemon/1" />} path="*" />
+            </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
