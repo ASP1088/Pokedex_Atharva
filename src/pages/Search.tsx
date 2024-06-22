ede64cc6194f243f840a175b76e3a10a078ea702
Atharva pawar <atharvapawar2018@gmail.com>
2024-06-22 17:07:16 +0530

    Search page functionality and styling completed successfully

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import  { useEffect } from 'react'
 import Wrapper from '../sections/Wrapper'
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { getInitialPokemonData } from '../app/reducers/getInitialPokemonData';
+import { getPokemonsData } from '../app/reducers/getPokemonsData';
+import PokemonCardGrid from '../components/PokemonCardGrid';
+import { Value } from 'sass';
+import { debounce } from '../utils/Debounce';
 
 const Search = () => {
+  const dispatch = useAppDispatch();
+  const { allPokemon,randomPokemons } = useAppSelector(({ pokemon }) => pokemon);
+
+  useEffect(() => {
+    dispatch(getInitialPokemonData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (allPokemon) {
+      const clonedPokemons = [...allPokemon];
+      const randomPokemonsId = clonedPokemons
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 20);
+      dispatch(getPokemonsData(randomPokemonsId));
+    }
+  }, [allPokemon, dispatch]);
+  
+  const handleChamge = debounce((value:string)=>getPokemon(value),300)
+
+  const getPokemon = async(value:string) =>{
+    if(value.length){
+      const pokemons = allPokemon?.filter((pokemon)=> 
+        pokemon.name.includes(value.toLowerCase())
+    );
+    dispatch(getPokemonsData(pokemons!));
+    }else{
+      const clonedPokemons = [...(allPokemon as [])];
+      const randomPokemonsId = clonedPokemons
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 20);
+      dispatch(getPokemonsData(randomPokemonsId));
+    }
+  }
+
   return (
-    <div>Search</div>
+    <>
+      <div className="search">
+        <input type="text" 
+        className="pokemon-searchbar"
+        placeholder="Search Pokemon" 
+        onChange={(e)=> handleChamge(e.target.value)}
+        />
+        <PokemonCardGrid pokemons={randomPokemons!}/>
+      </div>
+    </>
   )
 }
 
